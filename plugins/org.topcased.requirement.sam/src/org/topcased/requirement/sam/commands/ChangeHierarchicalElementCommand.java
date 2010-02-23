251fc0a7285131f63973b7b955db6f6d99111bca
maudrain <>
2010-02-23 16:06:35 +0000

    Add: New resolvers specific to SAM including: -MoveElement -MovePort -Global Change from a flow

---
+++ plugins/org.topcased.requirement.sam/src/org/topcased/requirement/sam/commands/ChangeHierarchicalElementCommand.java
@@ -0,0 +1,109 @@
+/***********************************************************************************************************************
+ * Copyright (c) 2008,2009 Communication & Systems.
+ * 
+ * All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Sebastien GABEL (CS) - initial API and implementation
+ * 
+ **********************************************************************************************************************/
+package org.topcased.requirement.sam.commands;
+
+import org.eclipse.emf.common.command.AbstractCommand;
+import org.eclipse.emf.ecore.EObject;
+import org.topcased.modeler.sam.util.GlobalFlowChangesManager;
+import org.topcased.modeler.sam.util.TransformationInfo;
+import org.topcased.requirement.HierarchicalElement;
+import org.topcased.requirement.core.utils.RequirementUtils;
+
+/**
+ * Command changing the {@link HierarchicalElement} if necessary<br>
+ * 
+ * Creation : 11th june 2009.<br>
+ * 
+ * @author <a href="mailto:sebastien.gabel@c-s.fr">Sebastien GABEL</a>
+ * @since Topcased 3.0.0
+ * @see {@link GlobalFlowChangesWithRequirementCommand}
+ * @see {@link GlobalFlowChangesManager}
+ */
+public class ChangeHierarchicalElementCommand extends AbstractCommand
+{
+    /**
+     * Model object after the transformation
+     */
+    private TransformationInfo transformation;
+
+    /**
+     * The {@link HierarchicalElement} to change
+     */
+    private HierarchicalElement hierarchicalElt;
+
+    /**
+     * Constructor
+     * 
+     * @param info The information related to the transformation to perform.
+     */
+    public ChangeHierarchicalElementCommand(TransformationInfo info)
+    {
+        transformation = info;
+
+    }
+
+    /**
+     * @see org.eclipse.emf.common.command.Command#redo()
+     */
+    public void redo()
+    {
+        EObject before = transformation.getBefore();
+        EObject after = transformation.getAfter();
+        if (hierarchicalElt == null)
+        {
+            hierarchicalElt = RequirementUtils.getHierarchicalElementFor(before);
+        }
+
+        if (hierarchicalElt != null)
+        {
+            hierarchicalElt.setElement(after);
+        }
+    }
+
+    /**
+     * @see org.eclipse.emf.common.command.AbstractCommand#undo()
+     */
+    @Override
+    public void undo()
+    {
+        EObject before = transformation.getBefore();
+        EObject after = transformation.getAfter();
+        if (hierarchicalElt != null)
+        {
+            hierarchicalElt.setElement(before);
+        }
+        if (hierarchicalElt == null)
+        {
+            if (after!=null)
+            {
+            hierarchicalElt = RequirementUtils.getHierarchicalElementFor(after);
+            }
+        }
+    }
+
+    /**
+     * @see org.eclipse.emf.common.command.Command#execute()
+     */
+    public void execute()
+    {
+        redo();
+    }
+
+    /**
+     * @see org.eclipse.emf.common.command.AbstractCommand#canExecute()
+     */
+    @Override
+    public boolean canExecute()
+    {
+        return transformation != null;
+    }
+
+}
---
+++ plugins/org.topcased.requirement.sam/src/org/topcased/requirement/sam/commands/MoveElementCommandResolver.java
@@ -0,0 +1,106 @@
+/*****************************************************************************
+ * Copyright (c) 2010 Communication & Systems
+ * 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors : Maxime AUDRAIN (CS) - initial API and implementation
+ * 
+ *****************************************************************************/
+
+package org.topcased.requirement.sam.commands;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import org.eclipse.gef.commands.Command;
+import org.topcased.modeler.commands.CommandStack;
+import org.topcased.modeler.commands.EMFtoGEFCommandWrapper;
+import org.topcased.modeler.sam.commands.MoveElementCommand;
+import org.topcased.requirement.core.commands.AdditionalCommand;
+import org.topcased.requirement.core.commands.MoveHierarchicalElementCommand;
+
+/**
+ * This Class handle specific behaviour for requirements when a MoveElementCommand is executed.
+ *
+ * @author <a href="mailto:maxime.audrain@c-s.fr">Maxime AUDRAIN</a> * 
+ */
+public class MoveElementCommandResolver extends AdditionalCommand<MoveElementCommand>
+{
+
+    private Map<MoveElementCommand, EMFtoGEFCommandWrapper> commands;
+
+    public MoveElementCommandResolver()
+    {
+        this(MoveElementCommand.class);
+        commands = new HashMap<MoveElementCommand, EMFtoGEFCommandWrapper>();
+    }
+
+    public MoveElementCommandResolver(Class< ? super MoveElementCommand> clazz)
+    {
+        super(clazz);
+    }
+
+    /**
+     * @see org.topcased.requirement.core.commands.AdditionalCommand#pre_execute(java.util.List)
+     */
+    @Override
+    protected void post_execute(List<MoveElementCommand> moveCommands)
+    {
+
+        for (MoveElementCommand cmd : moveCommands)
+        {            
+            EMFtoGEFCommandWrapper commandWrapper = new EMFtoGEFCommandWrapper(new MoveHierarchicalElementCommand(cmd.getNewContainer(), cmd.getDroppedChildren()));
+            commandWrapper.execute();
+            commands.put(cmd, commandWrapper);
+        }
+
+    }
+
+    /**
+     * @see org.topcased.requirement.core.commands.AdditionalCommand#pre_redo(java.util.List)
+     */
+    @Override
+    protected void pre_redo(List<MoveElementCommand> moveCommands)
+    {
+        for (MoveElementCommand moveCommand : moveCommands)
+        {
+            EMFtoGEFCommandWrapper wrap = commands.get(moveCommand);
+            if (wrap != null)
+            {
+                wrap.redo();
+            }
+        }            
+    }
+
+    /**
+     * @see org.topcased.requirement.core.commands.AdditionalCommand#post_undo(java.util.List)
+     */
+    @Override
+    protected void post_undo(List<MoveElementCommand> moveCommands)
+    {
+        for (ListIterator<MoveElementCommand> i = moveCommands.listIterator(moveCommands.size()); i.hasPrevious();)
+        {
+            MoveElementCommand moveCommand = i.previous();
+            EMFtoGEFCommandWrapper wrap = commands.get(moveCommand);
+            if (wrap != null)
+            {
+                wrap.undo();
+            }
+        }
+    }
+
+    /**
+     * @see org.topcased.requirement.core.commands.AdditionalCommand#getSpecificCommands(org.eclipse.gef.commands.Command,
+     *      java.lang.Class)
+     */
+    @Override
+    protected List<Object> getSpecificCommands(Command command, Class< ? > clazz)
+    {
+        return CommandStack.getCommands(command, clazz);
+    }
+}
