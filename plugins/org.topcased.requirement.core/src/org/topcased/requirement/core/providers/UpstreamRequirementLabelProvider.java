e0e6f65e17f6203f3e28118fee615b398b827ca6
sgabel <>
2010-03-25 15:41:20 +0000

    Change code of this class : use JFaceFontRegistry to improve performances.

--- plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/providers/UpstreamRequirementLabelProvider.java
+++ plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/providers/UpstreamRequirementLabelProvider.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2008 TOPCASED consortium.
+ * Copyright (c) 2008,2010 Communication & Systems.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,23 +7,25 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *  	Christophe Mertz (CS) <christophe.mertz@c-s.fr>
+ *      Christophe Mertz (CS) <christophe.mertz@c-s.fr>
  *    
  ******************************************************************************/
 package org.topcased.requirement.core.providers;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.jface.resource.StringConverter;
 import org.eclipse.jface.viewers.IFontProvider;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.FontData;
-import org.eclipse.swt.widgets.Display;
+import org.topcased.modeler.utils.Utils;
 import org.topcased.requirement.core.utils.RequirementUtils;
+
 import ttm.Requirement;
 
 /**
- * This customized label provider manages the font's style to applied to the requirement contained in the Upstream Page.
+ * This customized label provider manages the font's style to applied to the requirement contained in the Upstream Page.<br>
+ * 
+ * Updated : 11 march 2010<br>
  * 
  * @author <a href="mailto:christophe.mertz@c-s.fr">Christophe Mertz</a>
  * @author <a href="mailto:sebastien.gabel@c-s.fr">Sebastien GABEL</a>
@@ -31,20 +33,22 @@ import ttm.Requirement;
 public class UpstreamRequirementLabelProvider extends AdapterFactoryLabelProvider implements IFontProvider
 {
 
-    private Font boldItalicFont = null;
+    /** Font for covered upstream requirements */
+    private Font boldItalicFont;
 
-    private Font initialFont = null;
+    /** Font for not covered upstream requirements */
+    private Font initialFont;
 
     /**
      * Constructor
      * 
-     * @param adapterFactory The adapter factory to use
+     * @param adapterFactory The adapter factory to use into this provider.
      */
     public UpstreamRequirementLabelProvider(AdapterFactory adapterFactory)
     {
         super(adapterFactory);
-        initialFont = getStyledFont(Display.getCurrent().getSystemFont(), SWT.NONE);
-        boldItalicFont = getStyledFont(Display.getCurrent().getSystemFont(), SWT.BOLD | SWT.ITALIC);
+        initialFont = Utils.getFont(StringConverter.asFontData("Sans-regular-10")); //$NON-NLS-1$
+        boldItalicFont = Utils.getFont(StringConverter.asFontData("Sans-bold italic-10")); //$NON-NLS-1$
     }
 
     /**
@@ -61,39 +65,4 @@ public class UpstreamRequirementLabelProvider extends AdapterFactoryLabelProvide
         }
         return initialFont;
     }
-
-    /**
-     * Get a styled font
-     * 
-     * @param originalFont
-     * @param style
-     * 
-     * @return the font
-     */
-    private Font getStyledFont(Font originalFont, int style)
-    {
-        FontData[] fontData = originalFont.getFontData();
-        for (int i = 0; i < fontData.length; i++)
-        {
-            fontData[i].setStyle(fontData[i].getStyle() | style);
-        }
-        return new Font(Display.getDefault(), fontData);
-    }
-
-    /**
-     * @see org.eclipse.jface.viewers.BaseLabelProvider#dispose()
-     */
-    public void dispose()
-    {
-        super.dispose();
-        if (boldItalicFont != null)
-        {
-            boldItalicFont.dispose();
-        }
-        if (initialFont != null)
-        {
-            initialFont.dispose();
-        }
-    }
-
 }
