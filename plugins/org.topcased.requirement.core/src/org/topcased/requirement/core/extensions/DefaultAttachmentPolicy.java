991607f84008642f80277a718c1a5cd74062af51
tfaure <>
2010-05-06 13:18:19 +0000

    fix to avoid resourcetImpl creation

--- plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/extensions/DefaultAttachmentPolicy.java
+++ plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/extensions/DefaultAttachmentPolicy.java
@@ -20,7 +20,6 @@ import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.RemoveCommand;
 import org.eclipse.emf.edit.command.SetCommand;
@@ -259,11 +258,10 @@ public class DefaultAttachmentPolicy implements IModelAttachmentPolicy
     public RequirementProject getRequirementProjectFromTargetDiagram(Diagrams diagram)
     {
         Property property = getProperty(diagram);
-        
-        if (property != null)
+        if (property != null && diagram.eResource() != null && diagram.eResource().getResourceSet() != null)
         {
             String uriRequirementModel = URI.createURI(property.getValue()).trimFragment().resolve(property.eResource().getURI()).toString();
-            return (RequirementProject) new ResourceSetImpl().getResource(URI.createURI(uriRequirementModel), true).getContents().get(0);
+            return (RequirementProject) diagram.eResource().getResourceSet().getResource(URI.createURI(uriRequirementModel), true).getContents().get(0);
         }
         return null;
     }
