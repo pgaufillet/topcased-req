f87d9ee3f07aed5207ecd9b141567bda142f1c1d
sgabel <>
2010-10-11 08:38:10 +0000

    [#3264] [REQ+SAM] Incomplete behavior when a Flow is deleted from the model.

--- plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/resolvers/DeleteCommandResolver.java
+++ plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/resolvers/DeleteCommandResolver.java
@@ -9,43 +9,38 @@
  * Contributors:
  *  Tristan FAURE (ATOS ORIGIN INTEGRATION) - Initial API and implementation
  *  Maxime AUDRAIN (CS) - API changes
+ *  Sebastien GABEL (CS) - evolutions
  *
  *****************************************************************************/
-
 package org.topcased.requirement.core.resolvers;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
-import org.eclipse.gef.commands.Command;
-import org.eclipse.gef.commands.CompoundCommand;
-import org.topcased.modeler.commands.CommandStack;
 import org.topcased.modeler.commands.DeleteModelCommand;
 import org.topcased.modeler.commands.EMFtoGEFCommandWrapper;
 import org.topcased.requirement.core.commands.RemoveRequirementCommand;
 
 /**
- * This Class handle specific behaviour for requirements when a DeleteModelCommand is executed.
+ * This Class handle specific behaviour for requirements when a <b>DeleteModelCommand</b> is executed.
  * 
  * @author <a href="tristan.faure@atosorigin.com">Tristan FAURE</a>
  * @author <a href="mailto:maxime.audrain@c-s.fr">Maxime AUDRAIN</a>
- * 
+ * @author <a href="mailto:sebastien.gabel@c-s.fr">Sebastien GABEL</a>
  */
 public class DeleteCommandResolver extends AdditionalCommand<DeleteModelCommand>
 {
 
-    private Map<DeleteModelCommand, CompoundCommand> commands;
+    private Map<DeleteModelCommand, EMFtoGEFCommandWrapper> commands;
 
     public DeleteCommandResolver()
     {
         this(DeleteModelCommand.class);
-        commands = new HashMap<DeleteModelCommand, CompoundCommand>();
+        commands = new HashMap<DeleteModelCommand, EMFtoGEFCommandWrapper>();
     }
 
     public DeleteCommandResolver(Class< ? super DeleteModelCommand> clazz)
@@ -59,20 +54,13 @@ public class DeleteCommandResolver extends AdditionalCommand<DeleteModelCommand>
     @Override
     protected void pre_execute(List<DeleteModelCommand> deleteModelCommands)
     {
-
         for (DeleteModelCommand deleteModelCommand : deleteModelCommands)
         {
             Collection<EObject> eObjects = deleteModelCommand.getObjectsDeleting();
-            CompoundCommand command = new CompoundCommand();
-            for (EObject e : eObjects)
-            {
-                RemoveRequirementCommand com = new RemoveRequirementCommand(AdapterFactoryEditingDomain.getEditingDomainFor(e), e);
-                command.add(new EMFtoGEFCommandWrapper(com));
-            }
-            command.execute();
-            commands.put(deleteModelCommand, command);
+            EMFtoGEFCommandWrapper deleteCmd = new EMFtoGEFCommandWrapper(new RemoveRequirementCommand(getModeler().getEditingDomain(), eObjects));
+            deleteCmd.execute();
+            commands.put(deleteModelCommand, deleteCmd);
         }
-
     }
 
     /**
@@ -83,10 +71,10 @@ public class DeleteCommandResolver extends AdditionalCommand<DeleteModelCommand>
     {
         for (DeleteModelCommand deleteModelCommand : deleteModelCommands)
         {
-            CompoundCommand compound = commands.get(deleteModelCommand);
-            if (compound != null)
+            EMFtoGEFCommandWrapper wrap = commands.get(deleteModelCommand);
+            if (wrap != null)
             {
-                compound.redo();
+                wrap.redo();
             }
         }
     }
@@ -100,58 +88,17 @@ public class DeleteCommandResolver extends AdditionalCommand<DeleteModelCommand>
         for (ListIterator<DeleteModelCommand> i = deleteModelCommands.listIterator(deleteModelCommands.size()); i.hasPrevious();)
         {
             DeleteModelCommand deleteModelCommand = i.previous();
-            CompoundCommand compound = commands.get(deleteModelCommand);
-            if (compound != null)
+            EMFtoGEFCommandWrapper wrap = commands.get(deleteModelCommand);
+            if (wrap != null)
             {
-                compound.undo();
+                wrap.undo();
             }
         }
     }
 
     /**
-     * @see org.topcased.requirement.core.resolvers.AdditionalCommand#getSpecificCommands(org.eclipse.gef.commands.Command,
-     *      java.lang.Class)
+     * @see org.topcased.requirement.core.resolvers.AdditionalCommand#dipose()
      */
-    @Override
-    protected List<Object> getSpecificCommands(Command command, Class< ? > clazz)
-    {
-        List<Object> result = new LinkedList<Object>();
-
-        // deals with DeleteModelContentCommand (specific behaviour)
-        if (command instanceof CompoundCommand)
-        {
-            CompoundCommand compound = (CompoundCommand) command;
-            List< ? > commands = compound.getCommands();
-            for (Object o : commands)
-            {
-                // if we got a compound command with one or more deleteModelCommand, we add the command to the result
-                if (o instanceof DeleteModelCommand)
-                {
-                    result.add((DeleteModelCommand) o);
-                }
-                else
-                {
-                    // same algorithm than CommandStack.getCommands
-                    List<Object> tmp = CommandStack.getCommands((Command) o, clazz);
-                    if (!(tmp.isEmpty()))
-                    {
-                        result.addAll(tmp);
-                    }
-                }
-            }
-        }
-        else
-        {
-            // same algorithm than CommandStack.getCommands
-            List<Object> tmp = CommandStack.getCommands(command, clazz);
-            if (!(tmp.isEmpty()))
-            {
-                result.addAll(tmp);
-            }
-        }
-        return result;
-    }
-    
     @Override
     public void dipose()
     {
@@ -161,5 +108,4 @@ public class DeleteCommandResolver extends AdditionalCommand<DeleteModelCommand>
             commands.clear();
         }
     }
-    
 }
