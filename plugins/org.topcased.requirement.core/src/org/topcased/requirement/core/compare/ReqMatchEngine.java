03cf1222fb9d331e93a3119de659df97739b380b
aradouan <>
2012-12-19 14:15:05 +0000

    [#4321] Adding an attribute comparison based on qualified name and value

--- plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/compare/ReqMatchEngine.java
+++ plugins/org.topcased.requirement.core/src/org/topcased/requirement/core/compare/ReqMatchEngine.java
@@ -16,19 +16,19 @@ import org.eclipse.emf.compare.match.engine.GenericMatchEngine;
 import org.eclipse.emf.compare.match.engine.internal.EcoreIDSimilarityChecker;
 import org.eclipse.emf.ecore.EObject;
 
+import ttm.Attribute;
 import ttm.Document;
 import ttm.HierarchicalElement;
+import ttm.Requirement;
 import ttm.Section;
 
 public class ReqMatchEngine extends GenericMatchEngine
 {
-
-    @Override
+    @Override   
     protected AbstractSimilarityChecker prepareChecker()
     {
         return new EcoreIDSimilarityChecker(filter, super.prepareChecker())
         {
-           
             @Override
             public boolean isSimilar(EObject obj1, EObject obj2) throws FactoryException 
             {
@@ -46,6 +46,33 @@ public class ReqMatchEngine extends GenericMatchEngine
             @Override
             public double absoluteMetric(EObject obj1, EObject obj2) throws FactoryException
             {
+                if (obj1 instanceof Attribute && obj2 instanceof Attribute){
+                    Attribute a1 = (Attribute) obj1 ;
+                    Attribute a2 = (Attribute) obj2 ;
+                    if (a1.eContainer() instanceof Requirement && a2.eContainer() instanceof Requirement)
+                    {
+                        Requirement req1 = (Requirement) a1.eContainer();
+                        Requirement req2 = (Requirement) a2.eContainer();
+                        if (getQualifiedName(req1).equals(getQualifiedName(req2))){
+                            if (a1.getName() != null && a1.getName().equals(a2.getName()) && a1.getValue() != null && a1.getValue().equals(a2.getValue()))
+                            {
+                                return 1 ;
+                            }
+                            else if (a1.getName() != null && a1.getName().equals(a2.getName()) && a1.getValue() == null && a2.getValue() == null)
+                            {
+                                return 1 ;
+                            }
+                            else if (a1.getName() == null && a2.getName()== null && a1.getValue() == null && a2.getValue() == null)
+                            {
+                                return 1 ;
+                            }
+                            else
+                            {
+                                return 0 ;
+                            }
+                        }
+                    }
+                }
                 double result = super.absoluteMetric(obj1, obj2);
                 if (result != 1)
                 {
@@ -58,6 +85,27 @@ public class ReqMatchEngine extends GenericMatchEngine
         
     }
     
+    public static String getQualifiedName(Attribute s)
+    {
+        String qn = "";
+        if (s.getParent() instanceof HierarchicalElement)
+        {
+            HierarchicalElement h = (HierarchicalElement) s.getParent();
+            qn = getQualifiedName(h);
+        }
+        if (qn.length() != 0){
+            qn = qn + "::" ;
+        }
+        qn = qn + s.getName() + "::";
+        qn = qn + getValue(s);
+        return qn;
+    }
+    
+    private static String getValue(Attribute s)
+    {
+        return s.getValue();
+    }
+
     public static String getQualifiedName(HierarchicalElement s)
     {
         if (s == null || s.getIdent() == null)
